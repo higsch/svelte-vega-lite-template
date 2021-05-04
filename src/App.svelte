874b1ed6dc239ac4664578e3cfe38405b805a913
higsch <matthias@higsch.com>
2021-05-04 09:52:58 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import ScatterPlot from './components/ScatterPlot.svelte';
+
+  const xySet = [
+    {
+      x: 0,
+      y: 1
+    },
+    {
+      x: 2,
+      y: 3
+    },
+    {
+      x: 5,
+      y: 10
+    },
+    {
+      x: 10,
+      y: 8
+    },
+    {
+      x: 12,
+      y: 7
+    },
+  ];
+</script>
+
+<ScatterPlot
+  xySet={xySet}
+/>
+
+<style>
+</style>
---
+++ src/components/ScatterPlot.svelte
@@ -0,0 +1,56 @@
+<script>
+  import embed from 'vega-embed';
+
+  export let xySet;
+
+  const spec = {
+    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+    width: 'container',
+    height: 'container',
+    mark: {
+      type: 'point', // line
+      shape: 'diamond',
+      filled: true,
+      size: 300
+    },
+    encoding: {
+      x: {
+        field: 'x',
+        type: 'quantitative',
+        title: 'x values'
+      },
+      y: {
+        field: 'y',
+        type: 'quantitative',
+        title: 'y values'
+      }
+    }
+  };
+
+  let container;
+
+  $: if (xySet) embed(container, {
+    ...spec,
+    data: { values: xySet}
+  });
+</script>
+
+<div class="scatter-plot-container">
+  <div
+    class="scatter-plot"
+    bind:this={container}
+  >
+  </div>
+</div>
+
+<style>
+  .scatter-plot-container {
+    width: 100%;
+    height: 100%;
+  }
+
+  .scatter-plot-container > div {
+    width: 800px;
+    height: 600px;
+  }
+</style>
